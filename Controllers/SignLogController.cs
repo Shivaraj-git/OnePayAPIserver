1f4b97eb8a370f0578fca748748dd94432f23ea9
Muthukkumaran23C103 <muthukkumaran@student.tce.edu>
2025-12-18 16:00:32 +0530

    Update SignLogController.cs

--- Controllers/SignLogController.cs
+++ Controllers/SignLogController.cs
@@ -17,19 +17,19 @@ namespace OnePayAPI.Controllers
         [HttpPost]
         public IActionResult SignUp([FromBody] SignUpDTO signUpDTO)
         {
-            if (signUpDTO == null) return BadRequest("Invalid request");
+            if (signUpDTO == null) return BadRequest(new { success = false, message = "Invalid request" });
             var merchant = _signLogServices.SignUp(signUpDTO.FullName, signUpDTO.BusinessName, signUpDTO.Email, signUpDTO.PhoneNumber, signUpDTO.Password);
-            if (merchant == null) return BadRequest("Merchant with given email or phone number already exists");//400
-            return Ok("Merchant Registered Successfully");//200
+            if (merchant == null) return BadRequest(new { success = false, message = "Email or phone already in use" });//400
+            return Ok(new { success = true, message = "Merchant registered successfully" });//200
         }
         //Route: api/SignLog/Login
         [HttpPost]
         public IActionResult Login([FromBody] LoginDTO loginDTO)
         {
-            if (loginDTO == null) return BadRequest("Invalid request");
+            if (loginDTO == null) return BadRequest(new { success = false, message = "Invalid request" });
             var merchant = _signLogServices.Login(loginDTO.LoginInput, loginDTO.Password);
-            if (merchant == null) return BadRequest("Invalid login credentials");//400
-            return Ok("Login Successful");//200
+            if (merchant == null) return BadRequest(new { success = false, message = "Invalid credentials" });//400
+            return Ok(new { success = true, message = "Login successful" });//200
         }
     }
 }
